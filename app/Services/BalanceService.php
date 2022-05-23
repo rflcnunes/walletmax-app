e229dde04358a478815e2395c688a7d35a975d50
Rafael <rafaelcnunes@icloud.com>
2022-05-23 03:40:48 -0300

    Inserted BalanceService

---
+++ app/Services/BalanceService.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\Contracts\BalanceRepositoryInterface;
+
+class BalanceService
+{
+    private $balanceService;
+
+    public function __construct(BalanceRepositoryInterface $balanceService)
+    {
+        $this->balanceService = $balanceService;
+    }
+
+    /**
+     * @return BalanceRepositoryInterface
+     */
+    public function getCreditRepository(): BalanceRepositoryInterface
+    {
+        return $this->balanceService;
+    }
+
+    public function totalValueByUser($id)
+    {
+        $debits = $this->getCreditRepository()->getDebits($id);
+        $credits = $this->getCreditRepository()->getCredits($id);
+        return $debits - $credits;
+    }
+}
