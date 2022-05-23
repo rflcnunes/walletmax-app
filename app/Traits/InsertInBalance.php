769b63c173d141d39803efaf3e38170810ad5a5d
Rafael <rafaelcnunes@icloud.com>
2022-05-23 03:36:23 -0300

    Trait adjustment

--- app/Traits/InsertInBalance.php
+++ app/Traits/InsertInBalance.php
@@ -3,35 +3,43 @@
 namespace App\Traits;
 
 use App\Models\Balance;
+use App\Services\BalanceService;
+use App\Services\CreditService;
 use App\Services\DebitService;
 
 trait InsertInBalance
 {
     private $debitService;
+    private $creditService;
+    private $balanceService;
 
-    public function __construct(DebitService $debitService)
+    public function __construct(DebitService $debitService, CreditService $creditService, BalanceService $balanceService)
     {
         $this->debitService = $debitService;
+        $this->creditService = $creditService;
+        $this->balanceService = $balanceService;
     }
 
     public function insertDebitInBalance($debit)
     {
-        $id = $debit->user_id;
-
-        $actualValue = $this->debitService
-            ->getDebitRepository()
-            ->getBalance($id);
-
-        $oldValue = $actualValue - $debit->value;
-
         $actualDate = date('Y-m-d');
 
         $balance = new Balance();
         $balance->user_id = $debit->user_id;
         $balance->type = 'Debit';
         $balance->value = $debit->value;
-        $balance->total_before_transaction = $oldValue;
-        $balance->total_after_transaction = $actualValue;
+        $balance->date = $actualDate;
+        $balance->save();
+    }
+
+    public function insertCreditInBalance($credit)
+    {
+        $actualDate = date('Y-m-d');
+
+        $balance = new Balance();
+        $balance->user_id = $credit->user_id;
+        $balance->type = 'Credit';
+        $balance->value = $credit->value;
         $balance->date = $actualDate;
         $balance->save();
     }
