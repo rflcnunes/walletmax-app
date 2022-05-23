4b75e138b2ac2842031f08c7658ca981184d02b8
Rafael <rafaelcnunes@icloud.com>
2022-05-23 02:26:09 -0300

    Created debit in DebitController

--- app/Http/Controllers/DebitController.php
+++ app/Http/Controllers/DebitController.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\DebitRequest;
+use App\Http\Responses\ApiResponse;
+use App\Services\DebitService;
 use Illuminate\Http\Request;
 
 class DebitController extends Controller
 {
-    //
+    private $debitService;
+
+    public function __construct(DebitService $debitService)
+    {
+        $this->debitService = $debitService;
+    }
+
+    public function debit(DebitRequest $request)
+    {
+        try {
+            $value = $request->value;
+
+            $this->debitService->debit([
+                'user_id' => auth()->user()->id,
+                'value' => $request->value,
+            ]);
+
+            return new ApiResponse(true, 'Your debit of US$' . $value . ' has been successfully made!');
+
+        } catch (\Exception $exception) {
+            return new ApiResponse(false, 'Sorry, it was not possible to make the deposit!', $exception->getMessage(), 400);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\DebitController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -24,4 +25,8 @@ Route::controller(AuthController::class)->group(function () {
     Route::post('logout', 'logout');
     Route::post('refresh', 'refresh');
     Route::post('home', 'home');
+});
+
+Route::controller(DebitController::class)->group(function () {
+    Route::post('/debit', 'debit')->middleware('jwt.auth');
 });
