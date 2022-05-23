47598b47808cfa31aab07d3d9745a14f8a9ffb52
Rafael <58157026+rflcnunes@users.noreply.github.com>
2022-05-23 01:37:01 -0300

    Create UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,19 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        User::create(
+            [
+                'name' => 'Rafael Costa',
+                'email' => 'rafael@test.com',
+                'password' => bcrypt('123456'),
+            ],
+        );
+        User::create(
+            [
+                'name' => 'Jeremias Antunez',
+                'email' => 'jere@test.com',
+                'password' => bcrypt('123456'),
+            ],
+        );
     }
 }
