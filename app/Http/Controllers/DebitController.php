3fe65b3d1dea6efebaa4a06d955102c8ea1b7784
Rafael <rafaelcnunes@icloud.com>
2022-05-29 17:37:11 -0300

    Debt treatment inserted

--- app/Http/Controllers/DebitController.php
+++ app/Http/Controllers/DebitController.php
@@ -20,13 +20,15 @@ class DebitController extends Controller
         try {
             $value = $request->input('value');
 
-           $debit = $this->debitService->debit([
+            $debit = $this->debitService->debit([
                 'user_id' => auth()->user()->id,
                 'value' => $request->value,
             ]);
 
-            if ( !$debit === true || $value === 0) {
-                return new ApiResponse(false, 'Your debit was not successful.');
+            if (!$debit === true) {
+
+                return ($value === 0) ? new ApiResponse(false, 'Debit value must be greater than 0') : new ApiResponse(false, 'You do not have enough balance');
+
             }
 
             return new ApiResponse(true, 'Your debit was successful');
