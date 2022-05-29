291320697ff71abaa3fea94f268f5f8c93d3f68f
Rafael <rafaelcnunes@icloud.com>
2022-05-29 19:25:51 -0300

    Removed unnecessary request

--- app/Http/Controllers/BalanceController.php
+++ app/Http/Controllers/BalanceController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Http\Responses\ApiResponse;
 use App\Services\BalanceService;
-use Illuminate\Http\Request;
 
 class BalanceController extends Controller
 {
