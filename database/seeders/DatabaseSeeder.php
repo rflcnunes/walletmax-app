4908bc9d761620dd1d236e4098fda98f115ab9d5
Rafael <58157026+rflcnunes@users.noreply.github.com>
2022-06-04 22:54:23 -0300

    Inserted Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(UserSeeder::class);database/seeders/DatabaseSeeder.php
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
